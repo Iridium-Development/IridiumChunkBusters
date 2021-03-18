09f0ba4042551e6318d4c102c682a9233a9362c8
Peaches_MLG <PeachesMLG100@gmail.com>
2021-03-18 12:23:31 +0000

    Code cleanup

--- src/main/java/com/iridium/chunkbusters/gui/ConfirmationGUI.java
+++ src/main/java/com/iridium/chunkbusters/gui/ConfirmationGUI.java
@@ -2,22 +2,25 @@ package com.iridium.chunkbusters.gui;
 
 import com.cryptomorin.xseries.XMaterial;
 import com.iridium.chunkbusters.IridiumChunkBusters;
+import com.iridium.chunkbusters.database.ChunkBuster;
 import com.iridium.chunkbusters.utils.ItemStackUtils;
 import com.iridium.chunkbusters.utils.StringUtils;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.jetbrains.annotations.NotNull;
 
-@Getter
-public class ConfirmationGUI implements InventoryHolder {
+import java.util.Optional;
+
+public class ConfirmationGUI implements GUI {
 
     private final int size;
     private final Location location;
-    @Setter
     private boolean activated;
 
     public ConfirmationGUI(int size, Location location) {
@@ -36,5 +39,29 @@ public class ConfirmationGUI implements InventoryHolder {
         inventory.setItem(11, ItemStackUtils.makeItem(IridiumChunkBusters.getInstance().getConfiguration().no));
         inventory.setItem(15, ItemStackUtils.makeItem(IridiumChunkBusters.getInstance().getConfiguration().yes));
         return inventory;
+    }
+
+    @Override
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getSlot() == 15) {
+            ChunkBuster chunkBuster = new ChunkBuster(event.getWhoClicked().getUniqueId(), location.getChunk(), size, IridiumChunkBusters.getInstance().getConfiguration().startYWherePlaced ? location.getBlockY() : location.getWorld().getMaxHeight());
+            location.getBlock().setType(Material.AIR, false);
+
+            //We need to save the chunkBuster to the db first so the id gets generated so its not null
+            Bukkit.getScheduler().runTaskAsynchronously(IridiumChunkBusters.getInstance(), () -> {
+                IridiumChunkBusters.getInstance().getDatabaseManager().saveChunkBuster(chunkBuster);
+                IridiumChunkBusters.getInstance().getDatabaseManager().getChunkBusters().thenAccept(chunkBusters -> {
+                    Optional<ChunkBuster> chunkBusterOptional = chunkBusters.stream().filter(cb -> cb.getTime().equals(chunkBuster.getTime()) && cb.getChunk().equals(chunkBuster.getChunk())).findFirst();
+                    chunkBusterOptional.ifPresent(buster -> Bukkit.getScheduler().runTask(IridiumChunkBusters.getInstance(), buster::deleteChunks));
+                });
+            });
+
+            this.activated = true;
+            event.getWhoClicked().closeInventory();
+        } else if (event.getSlot() == 11) {
+            location.getBlock().setType(Material.AIR, false);
+            event.getWhoClicked().getInventory().addItem(IridiumChunkBusters.getInstance().getChunkBuster(size));
+            event.getWhoClicked().closeInventory();
+        }
     }
 }
--- src/main/java/com/iridium/chunkbusters/listeners/InventoryClickListener.java
+++ src/main/java/com/iridium/chunkbusters/listeners/InventoryClickListener.java
@@ -1,76 +1,17 @@
 package com.iridium.chunkbusters.listeners;
 
-import com.iridium.chunkbusters.IridiumChunkBusters;
-import com.iridium.chunkbusters.database.ChunkBuster;
-import com.iridium.chunkbusters.gui.ConfirmationGUI;
-import com.iridium.chunkbusters.gui.LogsGUI;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import com.iridium.chunkbusters.gui.GUI;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.inventory.InventoryHolder;
-
-import java.util.Optional;
 
 public class InventoryClickListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getClickedInventory() == null) return;
-        InventoryHolder inventoryHolder = event.getClickedInventory().getHolder();
-        if (inventoryHolder == null) return;
-        if (inventoryHolder instanceof ConfirmationGUI) {
-            ConfirmationGUI confirmationGUI = (ConfirmationGUI) inventoryHolder;
+        if (event.getClickedInventory() != null && event.getInventory().getHolder() != null && event.getInventory().getHolder() instanceof GUI) {
             event.setCancelled(true);
-            if (event.getSlot() == 15) {
-                ChunkBuster chunkBuster = new ChunkBuster(event.getWhoClicked().getUniqueId(), confirmationGUI.getLocation().getChunk(), confirmationGUI.getSize(), IridiumChunkBusters.getInstance().getConfiguration().startYWherePlaced ? confirmationGUI.getLocation().getBlockY() : confirmationGUI.getLocation().getWorld().getMaxHeight());
-                confirmationGUI.getLocation().getBlock().setType(Material.AIR, false);
-
-                //We need to save the chunkBuster to the db first so the id gets generated so its not null
-                Bukkit.getScheduler().runTaskAsynchronously(IridiumChunkBusters.getInstance(), () -> {
-                    IridiumChunkBusters.getInstance().getDatabaseManager().saveChunkBuster(chunkBuster);
-                    IridiumChunkBusters.getInstance().getDatabaseManager().getChunkBusters().thenAccept(chunkBusters -> {
-                        Optional<ChunkBuster> chunkBusterOptional = chunkBusters.stream().filter(cb -> cb.getTime().equals(chunkBuster.getTime()) && cb.getChunk().equals(chunkBuster.getChunk())).findFirst();
-                        chunkBusterOptional.ifPresent(buster -> Bukkit.getScheduler().runTask(IridiumChunkBusters.getInstance(), buster::deleteChunks));
-                    });
-                });
-
-                confirmationGUI.setActivated(true);
-                event.getWhoClicked().closeInventory();
-            } else if (event.getSlot() == 11) {
-                confirmationGUI.getLocation().getBlock().setType(Material.AIR, false);
-                event.getWhoClicked().getInventory().addItem(IridiumChunkBusters.getInstance().getChunkBuster(confirmationGUI.getSize()));
-                event.getWhoClicked().closeInventory();
-            }
-        } else if (inventoryHolder instanceof LogsGUI) {
-            LogsGUI logsGUI = (LogsGUI) inventoryHolder;
-            event.setCancelled(true);
-            if (logsGUI.getChunkBusters().containsKey(event.getSlot())) {
-                ChunkBuster chunkBuster = logsGUI.getChunkBusters().get(event.getSlot());
-                if (chunkBuster.getY() == 0 && IridiumChunkBusters.getInstance().getConfiguration().restoreChunkBusters) {
-                    chunkBuster.undo();
-                    Bukkit.getScheduler().runTaskAsynchronously(IridiumChunkBusters.getInstance(), () -> IridiumChunkBusters.getInstance().getDatabaseManager().deleteChunkBuster(chunkBuster));
-                    event.getWhoClicked().closeInventory();
-                }
-            } else if (event.getSlot() == 51 && logsGUI.isNext()) {
-                event.getWhoClicked().openInventory(new LogsGUI(logsGUI.getUuid(), logsGUI.getPage() + 1).getInventory());
-            } else if (event.getSlot() == 48 && logsGUI.isPrevious()) {
-                event.getWhoClicked().openInventory(new LogsGUI(logsGUI.getUuid(), logsGUI.getPage() - 1).getInventory());
-            }
+            ((GUI) event.getInventory().getHolder()).onInventoryClick(event);
         }
     }
-
-    @EventHandler
-    public void onInventoryClose(InventoryCloseEvent event) {
-        InventoryHolder inventoryHolder = event.getInventory().getHolder();
-        if (inventoryHolder instanceof ConfirmationGUI) {
-            ConfirmationGUI confirmationGUI = (ConfirmationGUI) inventoryHolder;
-            if (confirmationGUI.isActivated()) return;
-            confirmationGUI.getLocation().getBlock().setType(Material.AIR, false);
-            event.getPlayer().getInventory().addItem(IridiumChunkBusters.getInstance().getChunkBuster(confirmationGUI.getSize()));
-        }
-    }
-
 }
