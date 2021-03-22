d01963ed1626ac844143be260e1a22385d990803
Peaches_MLG <PeachesMLG100@gmail.com>
2021-03-22 14:24:15 +0000

    Lang changes

--- src/main/java/com/iridium/chunkbusters/configs/Messages.java
+++ src/main/java/com/iridium/chunkbusters/configs/Messages.java
@@ -5,12 +5,8 @@ public class Messages {
     public String noPermission = "%prefix% &7You don't have permission for that.";
     public String mustBeAPlayer = "%prefix% &7You must be a player to execute this command.";
     public String unknownCommand = "%prefix% &7Unknown Command, Try /chunkbusters help";
-    public String helpMessage = "&e&l * &7 %command% : &e %description%";
-    public String helpHeader = "&e&lIridiumChunkBusters: &eHelp";
-    public String helpfooter = "&e<< &7Page %page% of %maxpage% &e>>";
-    public String previousPage = "<<";
-    public String nextPage = ">>";
-    public String helpPageHoverMessage = "Click to go to page %page%";
+    public String helpMessage = "<GRADIENT:FFB61D>/cb %command%</GRADIENT:FF711D>&f: &7%description%";
+    public String helpHeader = "<GRADIENT:FFB61D>&lIridiumChunkBusters</GRADIENT:FF711D>: &eHelp";
     public String mustBeANumber = "%prefix% &7That argument was not a number";
-    public String unknownPlayer = "%prefix% &7Unknwon player.";
+    public String unknownPlayer = "%prefix% &7Unknown player.";
 }
