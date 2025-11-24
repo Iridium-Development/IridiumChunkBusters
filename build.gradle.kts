3293d53b245be991117bcad52f13ae618f9f7d5a
Peaches_MLG <PeachesMLG100@gmail.com>
2025-11-24 12:17:50 +0000

    Workflows + Gradle Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     `maven-publish`
-    id("io.github.goooler.shadow") version "8.1.8"
+    id("com.gradleup.shadow") version "9.2.2"
 }
 
 group = "com.iridium"
@@ -9,6 +9,8 @@ version = "1.0.9"
 description = "IridiumChunkBusters"
 
 repositories {
+    mavenCentral()
+    mavenLocal()
     maven("https://repo.mvdw-software.com/content/groups/public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
@@ -17,18 +19,16 @@ repositories {
     maven("https://nexus.iridiumdevelopment.net/repository/maven-releases/")
     maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://repo.rosewooddev.io/repository/public/")
-    maven("https://repo.jeff-media.com/public/")
-    mavenCentral()
 }
 
 dependencies {
     // Dependencies that we want to shade in
     implementation("org.jetbrains:annotations:26.0.1")
-    implementation("com.iridium:IridiumCore:2.0.9")
+    implementation("com.iridium:IridiumCore:2.0.11")
     implementation("org.bstats:bstats-bukkit:3.1.0")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("de.jeff_media:SpigotUpdateChecker:1.3.2")
+    implementation("com.jeff_media:SpigotUpdateChecker:3.0.4")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.36")
@@ -55,7 +55,7 @@ tasks {
         // Relocate dependencies
         relocate("com.iridium.iridiumcore")
         relocate("com.j256.ormlite")
-        relocate("de.jeff_media.updatechecker")
+        relocate("com.jeff_media.updatechecker")
         relocate("org.bstats")
         relocate("org.intellij.lang.annotations")
         relocate("org.jetbrains.annotations")
